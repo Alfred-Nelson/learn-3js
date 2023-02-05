fd163e4ea9f7cd7f584825ec6c3fa10426cdc246
Alfred Nelson <alfrednelson375@gmail.com>
2023-02-05 23:33:14 +0530

    Feature: Animated Sphere 🔵

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -24,7 +24,7 @@ const boxSize = 2
 const boxSkeleton = new THREE.BoxGeometry(boxSize, boxSize, boxSize)
 const boxSkin = new THREE.MeshBasicMaterial({ color: "blue" })
 const box = new THREE.Mesh(boxSkeleton, boxSkin)
-box.position.set(boxSize/2, boxSize/2, boxSize/2)
+box.position.set(boxSize/2, boxSize/2 + 0.01, boxSize/2)
 scene.add(box)
 
 // Adding plane
@@ -32,10 +32,25 @@ const planeSkelton = new THREE.PlaneGeometry(20,20)
 const planeSkin = new THREE.MeshBasicMaterial({ color: "white", side: THREE.DoubleSide })
 const plane = new THREE.Mesh(planeSkelton, planeSkin)
 plane.rotateX(-0.5 * Math.PI)
-plane.position.set(0, -0.1, 0)
 scene.add(plane)
 
 const gridHelper = new THREE.GridHelper(20)
 scene.add(gridHelper)
 
-renderer.render(scene, camera)
+// Adding sphere
+const sphereSize = 1
+let bounceStep = 0
+const bounceSpeed = 0.08
+const sphereSkelton = new THREE.SphereGeometry(sphereSize, 50, 50);
+const sphereSkin = new THREE.MeshBasicMaterial({ color: "red", })
+const sphere = new THREE.Mesh(sphereSkelton, sphereSkin)
+scene.add(sphere)
+sphere.position.set(5, sphereSize + 0.01 ,2)
+
+const animate = () => {
+    bounceStep += bounceSpeed
+    sphere.position.y = (4 * Math.abs(Math.sin(bounceStep))) + sphereSize + 0.1
+    renderer.render(scene, camera)
+}
+
+renderer.setAnimationLoop(animate);
